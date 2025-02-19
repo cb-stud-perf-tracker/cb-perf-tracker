f5312d8d48c84f42b44bb5af6d43cec4f50f4bf7
Vikas Bandaru <vikas.bandaaru@gmail.com>
2025-02-19 09:11:42 +0530

    Admin: DB_Util Boilerplate

---
+++ phase1-servlets-jdbc/src/main/java/com/trackers/cbtracker/util/DB_Util.java
@@ -0,0 +1,14 @@
+package com.trackers.cbtracker.util;
+
+import java.sql.Connection;
+
+public class DB_Util {
+	public Connection getConnection(String dbName) {
+		Connection con = null;
+		
+		// TODO: Write the part of the code that creates SQL Connection object
+		// 		 with proper exception handling
+		
+		return con;
+	}
+}
